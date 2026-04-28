e41a5d6eb698dda824c86440ebcae8a6583302ca
Brock Sorenson <btsorens@asu.edu>
2026-04-28 11:38:33 -0700

    Updated app.py skeleton for basic query construction

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ Design and implement a database-backed web application that demonstrates perform
 
 ```bash
 python3 -m venv .venv
-source .venv/bin/activate
+source .venv/bin/activate # or .venv/Scripts/Activate on Windows OS
 pip install -r requirements.txt
 ```
--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# based on example app.py get_db_connection()
 def connectToDatabase():
     connection = psycopg2.connect(
         dbname="brocks_database",
@@ -13,3 +14,44 @@ def connectToDatabase():
     )
 
     return connection
+
+# based on example app.py index()
+@app.route("/", methods=["GET", "POST"])
+def index():
+    querySearchResults = []
+    executionTimer = None
+
+    if request.method == "POST":
+        queryValue = request.form.get("searchValue")
+        queryMode = request.form.get("queryMode")       # indexed vs non-indexed queries
+        queryOptions = request.form.get("queryOptions")     # singleQuery queries vs joinedQuery queries
+
+        databaseConnection = connectToDatabase()
+        cursor = databaseConnection.cursor()
+        
+        if queryMode == "indexed":
+            cursor.execute("CREATE INDEX IF NOT EXISTS column_index ON relation(column);")
+        
+        else:
+            cursor.execute("DROP INDEX IF EXISTS column_index;")
+
+        # Define search query based on options
+        sqlQueryOnDatabase = ""
+        if queryOptions == "singleQuery":
+            sqlQueryOnDatabase = "SELECT * FROM relation WHERE column=***"
+        else:
+            sqlQueryOnDatabase =    """
+                                    SELECT r1.column, r2.column
+                                    FROM relation1 r1
+                                    JOIN relation2 r2
+                                    ON r1.id = r2.fid
+                                    WHERE r1.column = ***
+                                    """
+                                    
+        cursor.execute(sqlQueryOnDatabase, (f"%{queryValue}%",))
+        queryResults = cursor.fetchall()
+
+        cursor.close()
+        databaseConnection.close()
+    
+    return render_template("frontEndQueryTool.html", returnedSearchResults=querySearchResults, executionTime=executionTimer)
