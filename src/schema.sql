6cac8f1972ac23e4ab8f475ca4985d11f6e6751c
Brock Sorenson <btsorens@asu.edu>
2026-04-28 18:34:49 -0700

    Updated schema with key constraints

--- src/schema.sql
+++ src/schema.sql
@@ -5,26 +5,26 @@ DROP TABLE IF EXISTS players;
 -- Create players table
 CREATE TABLE players (
     player_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    date_of_birth DATE,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    date_of_birth DATE NOT NULL CHECK (date_of_birth BETWEEN '1970-01-01' AND '2010-12-31'),
     hometown VARCHAR(100),
     state VARCHAR(2),
     position VARCHAR(10),
-    height INTEGER,
-    weight INTEGER,
-    jersey_number INTEGER
+    height INTEGER CHECK (height BETWEEN 68 and 80),
+    weight INTEGER CHECK (weight BETWEEN 150 and 300),
+    jersey_number INTEGER CHECK (jersey_number BETWEEN 0 and 99)
 );
 
 CREATE TABLE gamestats (
     stat_id SERIAL PRIMARY KEY,
-    player_id INTEGER REFERENCES players(player_id),
-    date_of_game DATE,
+    player_id INTEGER REFERENCES players(player_id) ON DELETE CASCADE,
+    date_of_game DATE NOT NULL,
     stadium_name VARCHAR(100),
-    rush_yards INTEGER,
-    num_rushes INTEGER,
-    rec_yards INTEGER,
-    num_receptions INTEGER,
-    points INTEGER,
-    snaps_played INTEGER
+    rush_yards INTEGER DEFAULT 0,
+    num_rushes INTEGER DEFAULT 0,
+    rec_yards INTEGER DEFAULT 0,
+    num_receptions INTEGER DEFAULT 0,
+    points INTEGER DEFAULT 0,
+    snaps_played INTEGER CHECK (snaps_played >= 0)
 );
